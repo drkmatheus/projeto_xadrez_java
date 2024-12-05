fb7b9dd5f379ca8b3ebeb47e04b6a225bf9d9d3d
drkmatheus <matheus.gcrosciati@gmail.com>
2024-12-05 17:36:32 -0300

    Primeira classe criada: Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,7 +1,11 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
+        Position position = new Position(0 ,1);
+        System.out.println(position);
     }
 }
